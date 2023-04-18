1ce50da9fb82b6fad14c6dc77e1d146d211540eb
Hoangson3101 <101043520+Hoangson3101@users.noreply.github.com>
2023-04-18 20:49:33 +0700

    Update bài giữa kì.py

--- bài giữa kì.py
+++ bài giữa kì.py
@@ -1,32 +1,31 @@
 CHARS = 256
 
 def getNextState(pat, M, state, x):
-
 	if state < M and x == ord(pat[state]):
 		return state+1
 
-	i=0	
-	for x in range(state,0,-1):
-		if ord(pat[x-1]) == x:
-			while(i<x-1):
-				if pat[i] != pat[state-x+1+i]:
+	i=0
+	for y in range(state,0,-1):
+		if ord(pat[y-1]) == x:
+			while(i<y-1):
+				if pat[i] != pat[state-y+1+i]:
 					break
 				i+=1
-			if i == x-1:
-				return x
+			if i == y-1:
+				return y
 	return 0
-
-def computeTF(pat, M):	
+def computeTF(pat, M):
 	global CHARS
 	TF = [[0 for i in range(CHARS)]\
 		for _ in range(M+1)]
+
 	for state in range(M+1):
 		for x in range(CHARS):
 			z = getNextState(pat, M, state, x)
 			TF[state][x] = z
 	return TF
-
-def search(pat, txt):	
+  
+def search(pat, txt):
 	global CHARS
 	M = len(pat)
 	N = len(txt)
@@ -38,7 +37,7 @@ def search(pat, txt):
 		if state == M:
 			print("Pattern found at index: {}".\
 				format(i-M+1))
-		
+	
 def main():
 	txt = "ABAACAADAABAAABAA"
 	pat = "AABA"
